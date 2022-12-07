23b80584c29b45a3aa1cf3b15ee1b016c582e48d
zhangming9502 <zhangming9502@gmail.com>
2022-12-07 10:35:36 +0800

    feat: 设置全局路由前缀

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  // 1. 固定路径：
+  // 可以匹配到 get请求，http://localhost:3000/app/list
+  @Get('list')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // 可以匹配到 post请求，http://localhost:3000/app/list
+  @Post('list')
+  create(): string {
+    return '';
+  }
+
+  // 2.通配符路径(?+* 三种通配符 )
+  // 可以匹配到 get请求, http://localhost:3000/app/user_xxx
+  @Get('user_*')
+  getUser(): string {
+    return 'getUser';
+  }
+
+  // 3.带参数路径
+  // 可以匹配到put请求，http://localhost:3000/app/list/xxxx
+  @Put('list/:id')
+  update(): string {
+    return 'update';
+  }
+
+  @Put('list/user')
+  updateUser() {
+    return {
+      userId: 1,
+    };
   }
 }
